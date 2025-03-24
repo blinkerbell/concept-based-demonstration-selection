302505ee13bcb00e6f0c96691dae40f3119e7537
Felix Riechmann <166625477+blinkerbell@users.noreply.github.com>
2025-03-24 15:54:29 +0100

    Update train.sh
    update training script for use with german-gpt2 and with call to random seed

--- train.sh
+++ train.sh
@@ -3,16 +3,6 @@ LR=1e-2
 N_PREFIX=10
 TASK=tune
 SPLIT=train
-MODEL=gpt2-large
-CUDA_VISIBLE_DEVICES=0 python train.py\
-    --gpt2 $MODEL\
-    --task $TASK\
-    --split $SPLIT\
-    --method $METHOD\
-    --n_gpu 1\
-    --tensorize_dir tensorized\
-    --out_dir checkpoints/$MODEL/$TASK-$SPLIT/prefix={$N_PREFIX}-{$METHOD}-lr={$LR}-initByVocab\
-    --batch_size 16\
-    --lr $LR\
-    --n_prefix_tokens $N_PREFIX\
-    --num_training_steps 100000\
+SEED=$(python -c "import json; print(json.load(open('config/random_seed.json'))['seed'])")
+MODEL="kkirchheim/german-gpt2-medium"
+CUDA_VISIBLE_DEVICES=0 python train.py --gpt2 $MODEL --task $TASK --split $SPLIT --seed $SEED --method $METHOD --n_gpu 1 --tensorize_dir tensorized --out_dir checkpoints/$MODEL/$TASK-$SPLIT/prefix={$N_PREFIX}-{$METHOD}-lr={$LR}-initByVocab --batch_size 16 --lr $LR --n_prefix_tokens $N_PREFIX --num_training_steps 100000\
