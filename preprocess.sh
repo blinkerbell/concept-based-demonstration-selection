9ffca11de2f402f431133d754aec74ec1e46760f
Felix Riechmann <166625477+blinkerbell@users.noreply.github.com>
2025-04-02 14:42:09 +0200

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,22 @@
-# enable virtual environment (as proposed in the original README)
-source "..\env\Scripts\activate"
+while getopts "k:" opt; do
+  case "$opt" in
+    k)
+      k="$OPTARG"
+      ;;
+    *)
+      echo "Usage: source script.sh -k <value>"
+      return 1
+      ;;
+  esac
+done
+
+if [ -z "$k" ]; then
+  echo "Error: -k argument is required."
+  echo "Usage: source script.sh -k <value>"
+  return 1
+fi
+
+echo "k = $k"
 
 # Generate random seed
 cd config
@@ -9,15 +26,10 @@ import json
 with open('random_seed.json', 'w') as f:
     json.dump({'seed': random.randint(0, 2**32 - 1)}, f)
 "
-
-# Reset the data directory
 cd ..
-rm -r -f data
 
 # Download and preprocess the data
 cd preprocess
 #huggingface-cli login --token="xyz"
-python run_liability.py --k 4 # adjust k as needed
-
-# back to main directory
+python run_liability.py --k $k
 cd ..
