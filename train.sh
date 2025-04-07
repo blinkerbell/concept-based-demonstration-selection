e49cba3413917f7d7fdb1b9e0dbd8f4a29ec216b
Felix Riechmann <166625477+blinkerbell@users.noreply.github.com>
2025-04-07 10:28:09 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ N_PREFIX=10
 TASK=tune
 SPLIT=train
 STEP=1000
-BATCH=32
+BATCH=64
 SEED=$(python -c "import json; print(json.load(open('config/random_seed.json'))['seed'])")
 MODEL="kkirchheim/german-gpt2-medium"
 CUDA_VISIBLE_DEVICES=0 python train.py --gpt2 $MODEL --task $TASK --split $SPLIT --seed $SEED --method $METHOD --n_gpu 1 --tensorize_dir tensorized --out_dir checkpoints/$MODEL/$TASK-$SPLIT/prefix={$N_PREFIX}-{$METHOD}-lr={$LR}-initByVocab --batch_size $BATCH --lr $LR --n_prefix_tokens $N_PREFIX --num_training_steps $STEP
