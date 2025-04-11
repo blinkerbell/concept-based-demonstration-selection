4bc4d286923955ee97ad28a23a5dd8e8ece2ab33
Felix Riechmann <166625477+blinkerbell@users.noreply.github.com>
2025-04-11 15:11:55 +0200

    Update tensorize.sh

--- tensorize.sh
+++ tensorize.sh
@@ -4,4 +4,14 @@ TASK=tune
 SPLIT=train
 SEED=$(python -c "import json; print(json.load(open('config/random_seed.json'))['seed'])")
 MODEL="kkirchheim/german-gpt2-medium"
-CUDA_VISIBLE_DEVICES=0 python train.py --task $TASK --split $SPLIT --tensorize_dir tensorized --seed $SEED --method $METHOD --n_prefix_tokens $N_PREFIX --do_tensorize --n_gpu 1 --n_process 10 --gpt2 $MODEL
+CUDA_VISIBLE_DEVICES=0 python train.py\
+  --task $TASK\
+  --split $SPLIT\
+  --tensorize_dir tensorized\
+  --seed $SEED\
+  --method $METHOD\
+  --n_prefix_tokens $N_PREFIX\
+  --do_tensorize\
+  --n_gpu 1\
+  --n_process 10\
+  --gpt2 $MODEL
